e6c7f0004198db70358da55258cbecd2afefe026
yRomulo <116182111+yRomulo@users.noreply.github.com>
2024-11-29 12:01:04 -0300

    itemCategory&Search

--- src/Pages/Stock/index.js
+++ src/Pages/Stock/index.js
@@ -1,66 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './styles.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 function Stock() {
   const [stock, setStock] = useState([]);
-  const [itemName, setItemName] = useState('');
-  const [itemQuantity, setItemQuantity] = useState('');
-  const [itemPrice, setItemPrice] = useState('');
+  const [itemName, setItemName] = useState("");
+  const [itemQuantity, setItemQuantity] = useState("");
+  const [itemPrice, setItemPrice] = useState("");
   const [editingIndex, setEditingIndex] = useState(null); // Índice do item em edição
+  const [itemCategory, setItemCategory] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sorting, setSorting] = useState({ field: "", ascending: true });
 
   useEffect(() => {
-    const storedStock = JSON.parse(localStorage.getItem('stock')) || [];
+    const storedStock = JSON.parse(localStorage.getItem("stock")) || [];
     setStock(storedStock);
   }, []);
 
   const handleAddItem = () => {
-    if (itemName && itemQuantity && itemPrice) {
+    if (itemName && itemQuantity && itemPrice && itemCategory) {
       const newItem = {
         name: itemName,
+        category: itemCategory,
         quantity: parseInt(itemQuantity, 10),
         price: parseFloat(itemPrice),
+
       };
       const updatedStock = [...stock, newItem];
       setStock(updatedStock);
-      localStorage.setItem('stock', JSON.stringify(updatedStock));
-      setItemName('');
-      setItemQuantity('');
-      setItemPrice('');
+      localStorage.setItem("stock", JSON.stringify(updatedStock));
+      setItemName("");
+      setItemQuantity("");
+      setItemPrice("");
     } else {
-      alert('Preencha todos os campos!');
+      alert("Preencha todos os campos!");
     }
   };
 
   const handleDeleteItem = (index) => {
     const updatedStock = stock.filter((_, idx) => idx !== index);
     setStock(updatedStock);
-    localStorage.setItem('stock', JSON.stringify(updatedStock));
+    localStorage.setItem("stock", JSON.stringify(updatedStock));
   };
 
   const handleEditItem = (index) => {
     setEditingIndex(index); // Define o índice do item sendo editado
+    setItemName(stock[index].name);
+    setItemQuantity(stock[index].quantity.toString());
+    setItemPrice(stock[index].price.toString());
+    setItemCategory(stock[index].category);
   };
 
   const handleSaveEdit = (index) => {
     const updatedStock = [...stock];
     updatedStock[index] = {
       name: itemName || stock[index].name,
-      quantity: itemQuantity ? parseInt(itemQuantity, 10) : stock[index].quantity,
+      quantity: itemQuantity
+        ? parseInt(itemQuantity, 10)
+        : stock[index].quantity,
       price: itemPrice ? parseFloat(itemPrice) : stock[index].price,
+      category: itemCategory || stock[index].category,
     };
     setStock(updatedStock);
-    localStorage.setItem('stock', JSON.stringify(updatedStock));
+    localStorage.setItem("stock", JSON.stringify(updatedStock));
     setEditingIndex(null); // Finaliza o modo de edição
-    setItemName('');
-    setItemQuantity('');
-    setItemPrice('');
+    setItemName("");
+    setItemQuantity("");
+    setItemPrice("");
+    setItemCategory("");
   };
+
+  const handleSort = (field) => {
+    const isAscending = sorting.field === field ? !sorting.ascending : true;
+    const sortedStock = [...stock].sort((a, b) => {
+      if (a[field] < b[field]) return isAscending ? -1 : 1;
+      if (a[field] > b[field]) return isAscending ? 1 : -1;
+      return 0;
+    });
+    setSorting({ field, ascending: isAscending });
+    setStock(sortedStock);
+  };
+
+  const filteredStock = stock.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className="stock-container">
       <div className="back">
-        <Link to="/Dashboard" className="btn">Voltar</Link>
+        <Link to="/Dashboard" className="btn">
+          Voltar
+        </Link>
       </div>
       <h2>Gerenciamento de Estoque</h2>
       <div className="add-item-container">
@@ -69,6 +99,13 @@ function Stock() {
           placeholder="Nome do Item"
           value={itemName}
           onChange={(e) => setItemName(e.target.value)}
+          className="input-field"
+        />
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={itemCategory}
+          onChange={(e) => setItemCategory(e.target.value)}
           className="input-field"
         />
         <input
@@ -85,20 +122,32 @@ function Stock() {
           onChange={(e) => setItemPrice(e.target.value)}
           className="input-field"
         />
-        <button onClick={handleAddItem} className="btn">Adicionar Item</button>
-      </div>
 
+        <button onClick={handleAddItem} className="btn">
+          Adicionar Item
+        </button>
+      </div>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Pesquisar por nome..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="input-field"
+        />
+      </div>
       <table className="stock-table">
         <thead>
           <tr>
-            <th>Nome</th>
-            <th>Quantidade</th>
-            <th>Preço</th>
+            <th onClick={() => handleSort("name")}>Nome</th>
+            <th onClick={() => handleSort("category")}>Categoria</th>
+            <th onClick={() => handleSort("quantity")}>Quantidade</th>
+            <th onClick={() => handleSort("price")}>Preço</th>
             <th>Ações</th>
           </tr>
         </thead>
         <tbody>
-          {stock.map((item, index) => (
+          {filteredStock.map((item, index) => (
             <tr key={index}>
               {editingIndex === index ? (
                 <>
@@ -108,6 +157,14 @@ function Stock() {
                       type="text"
                       defaultValue={item.name}
                       onChange={(e) => setItemName(e.target.value)}
+                      className="input-field"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      defaultValue={item.category}
+                      onChange={(e) => setItemCategory(e.target.value)}
                       className="input-field"
                     />
                   </td>
@@ -128,19 +185,40 @@ function Stock() {
                     />
                   </td>
                   <td>
-                    <button onClick={() => handleSaveEdit(index)} className="btn">Salvar</button>
-                    <button onClick={() => setEditingIndex(null)} className="btn">Cancelar</button>
+                    <button
+                      onClick={() => handleSaveEdit(index)}
+                      className="btn"
+                    >
+                      Salvar
+                    </button>
+                    <button
+                      onClick={() => setEditingIndex(null)}
+                      className="btn"
+                    >
+                      Cancelar
+                    </button>
                   </td>
                 </>
               ) : (
                 <>
                   {/* Campos normais */}
                   <td>{item.name}</td>
+                  <td>{item.category}</td>
                   <td>{item.quantity}</td>
                   <td>R${item.price}</td>
                   <td>
-                    <button onClick={() => handleEditItem(index)} className="btn">Editar</button>
-                    <button onClick={() => handleDeleteItem(index)} className="btn">Remover</button>
+                    <button
+                      onClick={() => handleEditItem(index)}
+                      className="btn"
+                    >
+                      Editar
+                    </button>
+                    <button
+                      onClick={() => handleDeleteItem(index)}
+                      className="btn"
+                    >
+                      Remover
+                    </button>
                   </td>
                 </>
               )}
